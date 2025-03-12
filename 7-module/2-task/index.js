a52a093033f5571771bc960adef6f1f895048e78
KirillDolbnya <kirilldolbnya6@gmail.com>
2025-03-13 00:34:19 +0300

    решил задачи 6 и 7 модуля

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,73 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
+
+  #elem = null;
+
   constructor() {
+    this.#elem = this.#render();
+  }
+
+  #template() {
+    return `
+    <div class="modal">
+      <div class="modal__overlay"></div>
+
+      <div class="modal__inner">
+        <div class="modal__header">
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>
+    </div>`;
+  }
+
+  #render() {
+    this.#elem = createElement(this.#template());
+
+    document.body.addEventListener('click', (event) => this.#closeEvent(event));
+    document.body.addEventListener('keydown', (event) => this.#closeEvent(event));
+
+    return this.#elem;
+  }
+
+  open() {
+    document.body.append(this.#elem);
+    document.body.classList.add('is-modal-open');
+  }
+
+  setTitle(title) {
+    this.#elem.querySelector('.modal__title').textContent = title;
+  }
+
+  setBody(content) {
+    const modalBody = this.#elem.querySelector('.modal__body');
+    modalBody.innerHTML = '';
+    this.#elem.querySelector('.modal__body').appendChild(content);
+  }
+
+  #closeEvent(event) {
+    if (event.target.closest('.modal__close') || event.key === 'Escape') {
+      this.#elem.remove();
+      document.body.classList.remove('is-modal-open');
+    }
+  }
+
+  close() {
+    this.#elem.remove();
+    document.body.classList.remove('is-modal-open');
+  }
+
+  get elem() {
+    return this.#elem;
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,83 @@
 export default class StepSlider {
+
+  #elem = null;
+
   constructor({ steps, value = 0 }) {
+    this.steps = steps ?? 5;
+    this.value = value;
+    this.#elem = this.#render();
   }
+
+  #template() {
+    let stepHTML = ``;
+    for (let i = 0; i < this.steps; i++) {
+      stepHTML += `<span class="${ i === 0 ? 'slider__step-active' : ''}" ></span>`;
+    }
+
+    return `
+      <div class="slider">
+
+        <div class="slider__thumb" style="left: 0;">
+          <span class="slider__value">0</span>
+        </div>
+
+        <div class="slider__progress" style="width: 0;"></div>
+
+        <div class="slider__steps">
+          ${ stepHTML }
+        </div>
+      </div>
+    `;
+  }
+
+  #render() {
+    const divElement = document.createElement('div');
+
+    divElement.innerHTML = this.#template();
+
+    this.#elem = divElement.firstElementChild;
+
+    this.#elem.addEventListener('click', (event) => this.#stepEvent(event));
+
+    this.#elem.addEventListener('click', (event) => this.#onClick(event));
+
+    return this.#elem;
+  }
+
+  #onClick(event) {
+    const newEvent = new CustomEvent('slider-change', { detail: this.value, bubbles: true });
+
+    const span = event.target.querySelector('span');
+
+    if (span) {
+      span.dispatchEvent(newEvent);
+    }
+
+    return newEvent;
+  }
+
+  #stepEvent(event) {
+    const slider = event.target.closest('.slider');
+    const progress = slider.querySelector('.slider__progress');
+    const thumb = slider.querySelector('.slider__thumb');
+    const index = slider.querySelector('.slider__value');
+
+    let left = event.clientX - slider.getBoundingClientRect().left;
+    let leftRelative = left / slider.offsetWidth;
+    let segments = this.steps - 1;
+    let approximateValue = leftRelative * segments;
+    let value = Math.round(approximateValue);
+    let valuePercents = value / segments * 100;
+
+
+    this.value = value;
+    index.textContent = value;
+    thumb.style.left = `${valuePercents}%`;
+    progress.style.width = `${valuePercents}%`;
+  }
+
+  get elem() {
+    return this.#elem;
+  }
+
 }
